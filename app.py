abc9be48d2661ffbc9f74352a69e700928063263
Ephraim Park <ephiepark@gmail.com>
2016-08-21 00:42:29 -0700

    moved models code from app.py into models.py

--- app.py
+++ app.py
@@ -7,18 +7,12 @@ import sys
 
 import logging
 
-from flask import (Flask, flash, Markup, redirect, render_template, request,
+from flask import (Flask, flash, redirect, render_template, request,
                    Response, session, url_for)
-from markdown import markdown
-from markdown.extensions.codehilite import CodeHiliteExtension
-from markdown.extensions.extra import ExtraExtension
-from micawber import bootstrap_basic, parse_html
-from micawber.cache import Cache as OEmbedCache
-from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 # from playhouse.postgres_ext import *
 
-
+from models import flask_db, Entry
 
 # Create a Flask WSGI app and configure it using values from the module.
 app = Flask(__name__)
@@ -30,104 +24,11 @@ app.logger.addHandler(stream_handler)
 
 # FlaskDB is a wrapper for a peewee database that sets up pre/post-request
 # hooks for managing database connections.
-flask_db = FlaskDB(app)
+flask_db.init_app(app)
 
 # The `database` is the actual peewee database, as opposed to flask_db which is
 # the wrapper.
 database = flask_db.database
-
-# Configure micawber with the default OEmbed providers (YouTube, Flickr, etc).
-# We'll use a simple in-memory cache so that multiple requests for the same
-# video don't require multiple network requests.
-oembed_providers = bootstrap_basic(OEmbedCache())
-
-
-class Entry(flask_db.Model):
-  title = CharField()
-  slug = CharField(unique=True)
-  content = TextField()
-  published = BooleanField(index=True)
-  timestamp = DateTimeField(default=datetime.datetime.now, index=True)
-
-  @property
-  def html_content(self):
-    """
-    Generate HTML representation of the markdown-formatted blog entry,
-    and also convert any media URLs into rich media objects such as video
-    players or images.
-    """
-    hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
-    extras = ExtraExtension()
-    markdown_content = markdown(self.content, extensions=[hilite, extras])
-    oembed_content = parse_html(
-      markdown_content,
-      oembed_providers,
-      urlize_all=True,
-      maxwidth=app.config['SITE_WIDTH'])
-    return Markup(oembed_content)
-
-  def save(self, *args, **kwargs):
-    # Generate a URL-friendly representation of the entry's title.
-    if not self.slug:
-      self.slug = re.sub('[^\w]+', '-', self.title.lower()).strip('-')
-    ret = super(Entry, self).save(*args, **kwargs)
-
-    # Store search content.
-    # self.update_search_index()
-    return ret
-
-  # def update_search_index(self):
-    # Create a row in the FTSEntry table with the post content. This will
-    # allow us to use SQLite's awesome full-text search extension to
-    # search our entries.
-  #     try:
-  #         fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
-  #     except FTSEntry.DoesNotExist:
-  #         fts_entry = FTSEntry(entry_id=self.id)
-  #         force_insert = True
-  #     else:
-  #         force_insert = False
-  #     fts_entry.content = '\n'.join((self.title, self.content))
-  #     fts_entry.save(force_insert=force_insert)
-
-  @classmethod
-  def public(cls):
-    return Entry.select().where(Entry.published == True)
-
-  @classmethod
-  def drafts(cls):
-    return Entry.select().where(Entry.published == False)
-
-  @classmethod
-  def search(cls, query):
-    words = [word.strip() for word in query.split() if word.strip()]
-    if not words:
-      # Return an empty query.
-      return Entry.select().where(Entry.id == 0)
-    else:
-      search = ' '.join(words)
-
-    # Query the full-text search index for entries matching the given
-    # search query, then join the actual Entry data on the matching
-    # search result.
-    return Entry.select().where(Entry.published == True)
-    # (FTSEntry
-    # .select(
-    #     FTSEntry,
-    #     Entry,
-    #     FTSEntry.rank().alias('score'))
-    # .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
-    # .where(
-    #     (Entry.published == True) &
-    #     (FTSEntry.match(search)))
-    # .order_by(SQL('score').desc()))
-
-# class FTSEntry(FTSModel):
-#     entry_id = IntegerField(Entry)
-#     content = TextField()
-#
-#     class Meta:
-#         database = database
 
 def login_required(fn):
   @functools.wraps(fn)
